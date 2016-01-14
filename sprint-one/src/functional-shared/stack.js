da4fc14868eef1f2820d88bf1ed583b6b902b737
sanfordcb <sanford0518@gmail.com>
2016-01-14 17:41:09 -0600

    Implement .pop method for functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -10,14 +10,18 @@ var Stack = function() {
   someInstance.push = stackMethods.push;
   someInstance.size = stackMethods.size;
 
-
-
   return someInstance;
 };
 
 var stackMethods = {};
 stackMethods.pop = function() {
-
+  if(this.count) {
+    this.count--;
+  }
+  var temp = this.storage[this.count];
+  delete this.storage[this.count];
+  return temp;
+  
 };
 stackMethods.push = function(value) {
   this.storage[this.count] = value;
