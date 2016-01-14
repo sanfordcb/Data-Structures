48275a44399dd44b3d9b318946684a65929ee118
sanfordcb <sanford0518@gmail.com>
2016-01-14 17:38:20 -0600

    Write function for Stack and implement .size and .push methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.count = 0;
+  someInstance.storage = {};
+
+  someInstance.pop = stackMethods.pop;
+  someInstance.push = stackMethods.push;
+  someInstance.size = stackMethods.size;
+
+
+
+  return someInstance;
 };
 
 var stackMethods = {};
+stackMethods.pop = function() {
+
+};
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+stackMethods.size = function() {
+  return this.count;
+};
 
 
