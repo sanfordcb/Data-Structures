878842b5dcfd5240e66a059b6432f757da299fca
sanfordcb <sanford0518@gmail.com>
2016-01-14 17:54:18 -0600

    Implement .dequeue method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -19,7 +19,17 @@ queueMethods.enqueue = function(value) {
   this.storage[this.count] = value;
   this.count++;
 };
-queueMethods.dequeue = function() {};
+queueMethods.dequeue = function() {
+  var temp = this.storage[0];
+  delete this.storage[0];
+  for (var key in this.storage) {
+    this.storage[key-1] = this.storage[key];
+  }
+  if (this.count) {
+    this.count--;
+  }
+  return temp;
+};
 queueMethods.size = function() {
   return this.count;
 };
