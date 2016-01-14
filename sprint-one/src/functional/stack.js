1041baee9caab4267d57ab187bc30748bbb151ca
sanfordcb <sanford0518@gmail.com>
2016-01-14 13:02:50 -0600

    Implement someInstance.size method in functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,6 +4,8 @@ var Stack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var count = 0;
+
   // Implement the methods below
   someInstance.push = function(value){
 
@@ -13,6 +15,7 @@ var Stack = function(){
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
