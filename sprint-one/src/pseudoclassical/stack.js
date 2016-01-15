fa3d832989868d2da9598ec40598035c76823cd3
sanfordcb <sanford0518@gmail.com>
2016-01-14 18:32:58 -0600

    Implement .pop method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -10,7 +10,16 @@ Stack.prototype.push = function(value) {
   this.storage[this.count] = value;
   this.count++;
 };
-Stack.prototype.pop = function() {};
+
+Stack.prototype.pop = function() {
+  if(this.count) {
+    this.count--;
+  }
+  var temp = this.storage[this.count];
+  delete this.storage[this.count];
+  return temp;
+};
+
 Stack.prototype.size = function() {
   return this.count;
 };
