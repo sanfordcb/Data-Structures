af1400d65a43bfe06e65e7948988be4c9b1f769b
sanfordcb <sanford0518@gmail.com>
2016-01-14 18:29:43 -0600

    Create Stack function and Stack.prototype methods, Implement .size method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+  
 };
 
+Stack.prototype.push = function(value) {};
+Stack.prototype.pop = function() {};
+Stack.prototype.size = function() {
+  return this.count;
+};
 
