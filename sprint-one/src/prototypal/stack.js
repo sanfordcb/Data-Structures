d44e6124431346ba358f8cab5d929f7bd3df933f
sanfordcb <sanford0518@gmail.com>
2016-01-14 18:06:56 -0600

    Create stack function and stack methods, implement .size method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  
+  someInstance.count = 0;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {};
+stackMethods.pop = function() {};
+stackMethods.size = function() {
+  return this.count;
+};
 
 
