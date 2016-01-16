52295c9ee366da0032ad7d7faee24c065ea0231f
sanfordcb <sanford0518@gmail.com>
2016-01-15 22:06:42 -0600

    Implement set methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[this._storage.length] = item;
 };
 
 setPrototype.contains = function(item){
+  var result = false;
+  _.each(this._storage, function(value) {
+    if (value === item) {
+      result = true;
+    }
+  });
+  return result;
 };
 
 setPrototype.remove = function(item){
+  var index = _.indexOf(this._storage, item);
+  if (index !== -1) {
+    this._storage.splice(index, 1);
+  }   
+  for (var i = index + 1; i < this._storage.length; i++) {
+    this._storage[i-1] = this._storage[i];
+  }
 };
 
 /*
