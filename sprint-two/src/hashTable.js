57c66b89cee0860693d42475700595fd0bf4a56c
sanfordcb <sanford0518@gmail.com>
2016-01-16 12:16:44 -0600

    Implement .insert and .retrieve methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,11 +5,12 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage[i];
 };
 
 HashTable.prototype.remove = function(k){
