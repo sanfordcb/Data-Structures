1075302cf42643b5cd24f9976b65457fa665e960
sanfordcb <sanford0518@gmail.com>
2016-01-15 21:10:59 -0600

    Implement .forEachNode method

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -55,6 +55,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode){
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this) {
+    if(this.hasOwnProperty(key)) { 
+      cb(key);
+    }
+  }
 };
 
 /*
