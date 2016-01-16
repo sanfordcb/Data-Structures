1c9f2b03e5a07ba59c6d21abbbd1552447031c73
sanfordcb <sanford0518@gmail.com>
2016-01-15 21:20:31 -0600

    Determine time complexity of methods

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -64,6 +64,13 @@ Graph.prototype.forEachNode = function(cb){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addNode: O(1)
+ contains: O(n)
+ removeNode: O(n^2)
+ hasEdge: O(n)
+ addEdge: O(1)
+ removeEdge: O(n)
+ forEachNode: O(n)A
  */
 
 
