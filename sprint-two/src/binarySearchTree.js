365b49acb5f94b9498ef423c912d7fb69bc68603
sanfordcb <sanford0518@gmail.com>
2016-01-16 18:07:52 -0600

    Add properties and methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,22 @@
 var BinarySearchTree = function(value){
+  
+  this.value = value;
+  this.left = [];
+  this.right = [];
+
 };
 
+BinarySearchTree.prototype.insert = function(value) {
 
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
