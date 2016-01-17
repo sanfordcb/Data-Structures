917774065ea4838be164486235ea3f8182ae58a2
sanfordcb <sanford0518@gmail.com>
2016-01-16 19:44:26 -0600

    Implement .insert method on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,13 +1,27 @@
 var BinarySearchTree = function(value){
-  
-  this.value = value;
-  this.left = [];
-  this.right = [];
+  var newTree = Object.create(BinarySearchTree.prototype);
 
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
+  return newTree;
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-
+  if(value > this.value) {
+    if(this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.insert.call(this.right, arguments[0]);
+    }
+  } else {
+    if(this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.insert.call(this.left, arguments[0]);
+    }
+  }
 };
 
 BinarySearchTree.prototype.contains = function(value) {
